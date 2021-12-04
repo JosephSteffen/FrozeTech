b05d5f75aa777f6dd36bc08c23628f19f8dcae85
JosephSteffen <josephsteffen2110@gmail.com>
2021-12-04 13:55:09 -0500

    Tweaked styles a bit. Added JS dropdown menu on mobile layout. Working on gallery bugfix and navigation fixes

--- imageslideshow.js
+++ imageslideshow.js
@@ -1,3 +1,5 @@
+//First gallery
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +28,44 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+//---------------------------------------------------
+
+//Second gallery
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active2", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active2";
+}
+
+function menu() {  
+  var drop = document.getElementById("navcontainer");  
+  if(drop.style.display ==="none") {  
+     drop.style.display ="flex";  
+  } else {  
+     drop.style.display ="none";  
+  }   
+}  
